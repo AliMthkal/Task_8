04b142835c4b25f4bdd51bd6628363252ec17b87
AliMthkal <153688393+AliMthkal@users.noreply.github.com>
2023-12-13 02:32:51 +0300

    Add files via upload

---
+++ Task8/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#btn-signin").addEventListener("click",function () {
+document.querySelector(".form").classList.add("active")
+});
+document.querySelector(".close-btn").addEventListener("click",function () {
+    document.querySelector(".form").classList.remove("active")
+    });
+    
